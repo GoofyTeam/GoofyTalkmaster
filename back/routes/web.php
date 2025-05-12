d5072493f1f2a802470729380dc231673737df9f
ALBUQUERQUE Adrien <adrialb95@gmail.com>
2025-05-12 23:51:58 +0200

    [TLKMSTR-31] feat(back): add Docker development configuration for backend

--- back/routes/web.php
+++ back/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'version' => '1.0.0',
+        'name' => 'Laravel API',
+        'description' => 'This is a simple API built with Laravel.',
+    ]);
 });
