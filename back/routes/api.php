d8845e80b82b85e549b0446f056190ff4002c485
Adrien Albuquerque <adrialb95@gmail.com>
2025-05-13 18:10:15 +0200

    [TLKMSTR-38] feat(back): add crsf cookie to the auth

--- back/routes/api.php
+++ back/routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
