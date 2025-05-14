6b7888830327c29f61ac9eb4679aff8ccffa34e1
ALBUQUERQUE Adrien <adrialb95@gmail.com>
2025-05-14 23:30:29 +0200

    [TLKMSTR-26] fix(back): remove useless phpdoc

--- back/app/Models/Room.php
+++ back/app/Models/Room.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
-    /** @use HasFactory<\Database\Factories\RoomFactory> */
     use HasFactory;
 
     protected $guarded = [
