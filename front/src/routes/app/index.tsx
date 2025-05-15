e60434688bb968c163c50788131787b62adc88b9
Hiteazel <khacminhbrandon.vo@gmail.com>
2025-05-16 01:29:57 +0200

    [TLKMSTR-46] feat(front): Homepage + filter component

--- front/src/routes/app/index.tsx
+++ front/src/routes/app/index.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from "@tanstack/react-router";
 import Homepage from "@/pages/homepage/Index";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app/")({
   component: Homepage,
