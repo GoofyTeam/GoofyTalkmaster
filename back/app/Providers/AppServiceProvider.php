8978ecfc1204270e64869ac9657ced8f312acf07
Adrien Albuquerque <adrialb95@gmail.com>
2025-05-15 11:23:49 +0200

    [TLKMSTR-63] fix(back): expose api docs on prod for everyone (YES I KNOW)

--- back/app/Providers/AppServiceProvider.php
+++ back/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
                 ui: '/api/docs',
                 document: '/api/docs/documentation.json',
             );
+
+        Gate::define('viewApiDocs', function () {
+            return true;
+        });
     }
 }
